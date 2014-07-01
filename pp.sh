f7e3b12fa2c212d1be9478bdf60947d4a598d720
Willy Stadnick <willy.stadnick@gmail.com>
2014-07-01 10:23:56 -0300

    Update pp.sh
    Translate messages

--- pp.sh
+++ pp.sh
@@ -3,8 +3,8 @@ while true; do
 	(
 		echo;
 		date;
-		zenity --entry --title="Olá piloto" --text="Qual seu nome?" --width="400" --height="300"
-	) >> pilotos.log &&
+		zenity --entry --title="Welcome pilot" --text="What is your name?" --width="400" --height="300"
+	) >> pilots.log &&
 	sleep 5m &&
-	zenity --warning --title="Tempo esgotado" --text="Por favor, promova seu co-piloto e retorne para a platéia." --width="400" --height="300"
+	zenity --warning --title="Time is up" --text="Please promote your co-pilot and return to the audience." --width="400" --height="300"
 done
