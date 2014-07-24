4e03c1eeee600b8edf97048d6289b0f635bad8bf
Willy Stadnick <willy@e3c.com.br>
2014-07-24 17:02:10 -0300

    Fix test driven development shell script

--- tdd.sh
+++ tdd.sh
@@ -4,10 +4,10 @@ while true; do
 	inotifywait -r ./ -e create,modify,delete &&
 	clear &&
 	phpunit --configuration phpunit.xml &&
-    (
-        if [ "$(git status --porcelain)" ]; then
-            git add -A . &&
-            git commit -m "$(zenity --entry --title=\"Congratulations pilot\" --text=\"What is the title of this commit?\" --width=\"400\" --height=\"300\")" --author="$(tail -1 pilots.log)"
-        fi
-    )
+	(
+        	if [ "`git status --porcelain`" ]; then
+			git add -A . &&
+			git commit -m "`zenity --entry --title="Congratulations pilot" --text="What is the title of this commit?" --width="400" --height="300"`" --author="`tail -1 pilots.log`"
+		fi
+	)
 done
