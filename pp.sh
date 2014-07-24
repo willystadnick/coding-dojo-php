2fd9c2f1a6e5a9328634789a57e16914ce5cb115
Willy Stadnick <willy.stadnick@gmail.com>
2014-07-24 13:56:46 -0300

    Update pp.sh
    Format pilot info to match git author spec

--- pp.sh
+++ pp.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 while true; do
+	name="$(zenity --entry --title=\"Welcome pilot\" --text=\"What is your name?\" --width=\"400\" --height=\"300\")"; 
+	email="$(zenity --entry --title=\"Welcome pilot\" --text=\"What is your email?\" --width=\"400\" --height=\"300\")"; 
 	(
-		echo;
-		date;
-		zenity --entry --title="Welcome pilot" --text="What is your name?" --width="400" --height="300"
+		echo; 
+		date; 
+		echo -n $name" <"$email">"
 	) >> pilots.log &&
 	sleep 5m &&
 	zenity --warning --title="Time is up" --text="Please promote your co-pilot and return to the audience." --width="400" --height="300"
