7102f9c38911cf063dad3125f643d1a068c19f10
Willy Stadnick <willy.stadnick@gmail.com>
2014-07-24 13:29:36 -0300

    Update tdd.sh
    Override the commit author

--- tdd.sh
+++ tdd.sh
@@ -7,15 +7,7 @@ while true; do
     (
         if [ "$(git status --porcelain)" ]; then
             git add -A . &&
-            git commit -m "`
-                zenity --entry --title="Congratulations pilot" --text="What is the title of this commit?" --width="400" --height="300" &&
-                (
-                    echo &&
-                    echo -n @ &&
-                    tail -1 pilots.log
-                ) ||
-                sleep 15s
-            `"
+            git commit -m "$(zenity --entry --title=\"Congratulations pilot\" --text=\"What is the title of this commit?\" --width=\"400\" --height=\"300\")" --author="$(tail -1 pilots.log)"
         fi
     )
 done
